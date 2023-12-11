1a47fd3fedca76b7e95be1d861954e7843d5e3d4
stefan <1119788227@qq.com>
2023-12-11 09:39:03 +0800

    a

---
+++ src/test/java/com/example/activiti8test/Activiti8TestApplicationTests.java
@@ -0,0 +1,81 @@
+package com.example.activiti8test;
+
+
+import org.activiti.api.process.model.ProcessDefinition;
+import org.activiti.api.process.runtime.ProcessRuntime;
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.api.task.runtime.TaskRuntime;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.activiti8test.utils.SecurityUtil;
+
+@SpringBootTest
+class Activiti8TestApplicationTests {
+
+    /**
+     * 使用配置类的方式创建表
+     * 结果是：表可以成功创建，但是运行结果是失败的
+     */
+    @Test
+    public void createByProcessEngineConfiguration() {
+        ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
+        processEngineConfiguration.setJdbcDriver("com.mysql.cj.jdbc.Driver")
+                .setJdbcUrl("jdbc:mysql://localhost:3306/activiti8?nullCatalogMeansCurrent=true")
+                .setJdbcUsername("root")
+                .setJdbcPassword("shuizhiyve1!");
+        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+        System.out.println("processEngine = " + processEngine);
+    }
+
+    /**
+     * 使用默认方法进行建表
+     */
+    @Test
+    void test() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        System.out.println("processEngine = " + processEngine);
+    }
+
+    @Autowired
+    private ProcessRuntime processRuntime;
+
+    @Autowired
+    private TaskRuntime taskRuntime;
+
+    @Autowired
+    private SecurityUtil securityUtil;
+
+    /**
+     * 查看流程定义
+     */
+    @Test
+    public void contextLoads() {
+        securityUtil.logInAs("system");
+        Page<ProcessDefinition> processDefinitionPage =
+                processRuntime.processDefinitions(Pageable.of(0, 10));
+        System.out.println("可用的流程定义数量：" + processDefinitionPage.getTotalItems());
+        for (org.activiti.api.process.model.ProcessDefinition pd : processDefinitionPage.getContent()) {
+            System.out.println("流程定义：" + pd);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
