b5126756e9927c1ecd2b86c6a9522d89c839d08a
stefan <1119788227@qq.com>
2023-12-11 09:18:59 +0800

    Add All

---
+++ src/main/java/com/example/activiti8test/Activiti8TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.activiti8test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activiti8TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activiti8TestApplication.class, args);
+    }
+
+}
